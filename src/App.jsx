7131ec227e91d411ba0e818e098fcc40483ee2ad
Sharmila Yadav <122268263+sharmilayadav11@users.noreply.github.com>
2024-08-28 23:06:40 +0300

    Created app and installed mui
    Created app and installed mui. Also installed font and icon from mui

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <>
+      <h1>Material UI AASHVI</h1>
+      <Button variant="contained" color="success">AASHVI</Button>
+
+    </>
+  )
+}
+
+export default App
