d0f81b907e71089ca9a1a604037b94f5443c0d46
Sharmila Yadav <122268263+sharmilayadav11@users.noreply.github.com>
2024-09-12 00:57:10 +0300

    Created text field for city name

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,7 +1,12 @@
+import TextField from '@mui/material/TextField';
+
 export default function SearchBox() {
     return (
         <div>
             <h3>Search for the Wearther</h3>
+            <form action="">
+                <TextField id="outlined-basic" label="City Name" variant="outlined" />
+            </form>
         </div>
     )
 }
