c770857f2e5220bd6a738b86e5b2232d07a171c2
Sharmila Yadav <122268263+sharmilayadav11@users.noreply.github.com>
2024-09-12 00:39:09 +0300

    Created new Componet SearchBox and imported it on app.jsx
    Created new Componet SearchBox and imported it on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
+import './App.css'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
-import './App.css'
 import Button from '@mui/material/Button';
+import SearchBox from './SearchBox';
 
 function App() {
   return (
     <>
-      <h1>Material UI Weather App</h1>
-      <Button variant="contained" color="success">AASHVI</Button>
-      <br />
-      <br />
-      <Button variant="contained" color="success">Bebu</Button>
+      <SearchBox />
+
 
     </>
   )
---
+++ src/SearchBox.jsx
@@ -0,0 +1,7 @@
+export default function SearchBox() {
+    return (
+        <div>
+            <h3>Search for the Wearther</h3>
+        </div>
+    )
+}
