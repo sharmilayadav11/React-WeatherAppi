1e248552c31e8b9116c4c778a23c42a7c39d7586
Sharmila Yadav <122268263+sharmilayadav11@users.noreply.github.com>
2024-10-18 11:46:27 +0300

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import './App.css'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import './App.css'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+
 
 import Button from '@mui/material/Button';
 import SearchBox from './SearchBox';
--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,11 +1,20 @@
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import "./SearchBox.css";
+
 
 export default function SearchBox() {
     return (
-        <div>
+        <div className='SearchBox'>
             <h3>Search for the Wearther</h3>
             <form action="">
                 <TextField id="city" label="City Name" variant="outlined" />
+                <br />
+                <br />
+                <Button variant="contained">
+                    Search
+                </Button>
+
             </form>
         </div>
     )
